fc0330633668a6eb74ecbdbe12ea7811f5df7384
Lin Sheng Jie <shengjie051018@gmail.com>
2026-08-04 02:48:16 +0800

    Updated create tables to match latest ERD design

--- create_warehouse.sql
+++ create_warehouse.sql
@@ -28,175 +28,217 @@ CREATE TABLE Branch_Dim (
     Branch_Key      NUMBER NOT NULL,
     Branch_ID       VARCHAR2(50) NOT NULL,
     Branch_Name     VARCHAR2(100) NOT NULL,
-    Full_Address    VARCHAR2(255) NOT NULL,
     City            VARCHAR2(100) NOT NULL,
     State           VARCHAR2(100) NOT NULL,
     Post_Code       VARCHAR2(20) NOT NULL,
+    Opening_Date    DATE NOT NULL,
+    Closing_Date    DATE,
     CONSTRAINT PK_Branch_Dim PRIMARY KEY (Branch_Key),
     CONSTRAINT FK_Branch_Dim_Branch_ID FOREIGN KEY (Branch_ID) REFERENCES Branches(BranchID)
 );
 
--- 3. Delivery Company Dimension
-CREATE TABLE Delivery_Company_Dim (
-    Delivery_Company_Key    NUMBER NOT NULL,
-    Company_ID              VARCHAR2(50) NOT NULL,
-    Company_Name            VARCHAR2(100) NOT NULL,
-    CONSTRAINT PK_Delivery_Company_Dim PRIMARY KEY (Delivery_Company_Key),
-    CONSTRAINT FK_Delivery_Comp_Dim_Company_ID FOREIGN KEY (Company_ID) REFERENCES Delivery_Companies(DeliveryCompanyID)
-);
-
--- 4. Supplier Dimension
+-- 3. Supplier Dimension
 CREATE TABLE Supplier_Dim (
     Supplier_Key    NUMBER NOT NULL,
     Supplier_ID     VARCHAR2(50) NOT NULL,
     Supplier_Name   VARCHAR2(100) NOT NULL,
+    Supplier_Status VARCHAR2(15) DEFAULT 'ACTIVE' NOT NULL,
+    City            VARCHAR2(80),
+    State           VARCHAR2(80),
+    PostCode        VARCHAR2(5),
     CONSTRAINT PK_Supplier_Dim PRIMARY KEY (Supplier_Key),
-    CONSTRAINT FK_Supplier_Dim_Supplier_ID (Supplier_ID) REFERENCES Suppliers(SupplierID)
+    CONSTRAINT FK_Supplier_Dim_Supplier_ID (Supplier_ID) REFERENCES Suppliers(SupplierID),
+    CONSTRAINT CK_Supplier_Dim_Status CHECK (Status IN ('ACTIVE', 'INACTIVE')),
+    CONSTRAINT CK_Supplier_Dim_PostCode CHECK (REGEXP_LIKE(PostCode, '^[0-9]{5}$'))
 );
 
--- 5. Customer Dimension
+-- 4. Customer Dimension
 CREATE TABLE Customer_Dim (
     Customer_Key        NUMBER NOT NULL,
     Customer_ID         VARCHAR2(50) NOT NULL,
     Customer_Name       VARCHAR2(100) NOT NULL,
-    Full_Address        VARCHAR2(255) NOT NULL,
-    City                VARCHAR2(100),
-    State               VARCHAR2(100),
-    Post_Code           VARCHAR2(20),
-    Is_Beneficiary      VARCHAR2(1) NOT NULL,
+    Is_Beneficiary      VARCHAR2(1) DEFAULT 'N' NOT NULL,
     Tier_Name           VARCHAR2(50) NOT NULL,
-    Point_Rate          NUMBER(5,2),
+    Join_Date           DATE,
     CONSTRAINT PK_Customer_Dim PRIMARY KEY (Customer_Key),
-    CONSTRAINT FK_Customer_Dim_Customer_ID FOREIGN KEY (Customer_ID) REFERENCES Customers(CustomerID)
+    CONSTRAINT FK_Customer_Dim_Customer_ID FOREIGN KEY (Customer_ID) REFERENCES Customers(CustomerID),
+    CONSTRAINT CK_Customer_Dim_MyKasih_Beneficiary CHECK (Is_Beneficiary in ('Y', 'N'))
 );
 
--- 6. Product Dimension
+-- 5. Product Dimension
 CREATE TABLE Product_Dim (
     Product_Key         NUMBER NOT NULL,
     Item_ID             VARCHAR2(50) NOT NULL,
     Item_Name           VARCHAR2(100) NOT NULL,
-    Description         VARCHAR2(255),
-    Unit_Measure        VARCHAR2(50) NOT NULL,
-    Unit_Cost           NUMBER(10,2) NOT NULL,
-    Is_Halal            VARCHAR2(1) NOT NULL,
-    Is_Mykasih_Eligible VARCHAR2(1) NOT NULL,
+    Unit_Measure        VARCHAR2(20) NOT NULL,
+    Unit_Price          NUMBER(10,2) NOT NULL,
+    Is_Halal            VARCHAR2(1) DEFAULT 'N' NOT NULL,
     Category_Name       VARCHAR2(100) NOT NULL,
     CONSTRAINT PK_Product_Dim PRIMARY KEY (Product_Key),
-    CONSTRAINT FK_Product_Dim_Item_ID FOREIGN KEY (Item_ID) REFERENCES Items(ItemID)
+    CONSTRAINT FK_Product_Dim_Item_ID FOREIGN KEY (Item_ID) REFERENCES Items(ItemID),
+    CONSTRAINT CK_Product_Dim_Unit_Measure CHECK (UnitMeasure IN ('BAG', 'BOTTLE', 'BOX', 'CAN', 'TIN', 'PACK', 'PCS')),
+    CONSTRAINT CK_Product_Dim_Is_Halal CHECK (Is_Halal IN ('Y', 'N'))
 );
 
--- 7. Staff Dimension
+-- 6. Staff Dimension
 CREATE TABLE Staff_Dim (
     Staff_Key       NUMBER NOT NULL,
     Staff_ID        VARCHAR2(50) NOT NULL,
     Staff_Name      VARCHAR2(100) NOT NULL,
     Role            VARCHAR2(50) NOT NULL,
     Hire_Date       DATE NOT NULL,
-    Branch_Name     VARCHAR2(100) NOT NULL,
+    Resigned_Date   DATE,
     CONSTRAINT PK_Staff_Dim PRIMARY KEY (Staff_Key),
-    CONSTRAINT FK_Staff_Dim_Staff_ID FOREIGN KEY (Staff_ID) REFERENCES Staffs(StaffID)
+    CONSTRAINT FK_Staff_Dim_Staff_ID FOREIGN KEY (Staff_ID) REFERENCES Staffs(StaffID),
+    CONSTRAINT CK_Staff_Dim_Role CHECK (Role IN ('MANAGER', 'CASHIER', 'STOCK_STAFF', 'ADMIN')),
 );
-
--- 8. Voucher Dimension
-CREATE TABLE Voucher_Dim (
-    Voucher_Key         NUMBER NOT NULL,
-    Voucher_ID          VARCHAR2(50) NOT NULL,
-    Voucher_Name        VARCHAR2(100) NOT NULL,
-    Voucher_Type        VARCHAR2(50) NOT NULL,
-    Point_Required      NUMBER(10) NOT NULL,
-    Discount_Amount     NUMBER(10,2) NOT NULL,
-    Min_Spend_Amount    NUMBER(10,2) NOT NULL,
-    Full_Desc           VARCHAR2(255),
-    Start_Date          DATE NOT NULL,
-    End_Date            DATE NOT NULL,
-    CONSTRAINT PK_Voucher_Dim PRIMARY KEY (Voucher_Key),
-    CONSTRAINT FK_Voucher_Dim_Voucher_ID FOREIGN KEY (Voucher_ID) REFERENCES Vouchers(Voucher_ID)
-);
-
 
 -- ==========================================
 -- FACT TABLES
 -- ==========================================
 
--- 9. Purchases Fact
+-- Purchases Fact
 CREATE TABLE Purchases_Fact (
     Supplier_Key        NUMBER NOT NULL,
-    Order_Date_Key      NUMBER NOT NULL,
+    PO_Date_Key         NUMBER NOT NULL,
     Branch_Key          NUMBER NOT NULL,
     Product_Key         NUMBER NOT NULL,
     Staff_Key           NUMBER NOT NULL,
-    Purchase_ID         NUMBER NOT NULL,
-    Quantity_Received   NUMBER(10) NOT NULL,
-    Unit_Cost           NUMBER(10,2) NOT NULL,
-    Line_Total          NUMBER(15,2) NOT NULL,
-    CONSTRAINT PK_Purchases_Fact PRIMARY KEY (Supplier_Key, Order_Date_Key, Branch_Key, Product_Key, Staff_Key, Purchase_ID),
+    Purchase_Order_ID   VARCHAR2(10) NOT NULL,
+    Received_Date_Key	NUMBER,
+    Quantity_Ordered	NUMBER(12) NOT NULL,
+    Quantity_Received   NUMBER(12) DEFAULT 0 NOT NULL,
+    Unit_Cost           NUMBER(12,2) NOT NULL,
+    Line_Total          NUMBER(14,2) NOT NULL,
+    CONSTRAINT PK_Purchases_Fact PRIMARY KEY (Supplier_Key, PO_Date_Key, Branch_Key, Product_Key, Staff_Key, Purchase_Order_ID),
     CONSTRAINT FK_Purch_Supplier FOREIGN KEY (Supplier_Key) REFERENCES Supplier_Dim(Supplier_Key),
-    CONSTRAINT FK_Purch_Date FOREIGN KEY (Order_Date_Key) REFERENCES Date_Dim(Date_Key),
+    CONSTRAINT FK_Purch_PO_Date FOREIGN KEY (PO_Date_Key) REFERENCES Date_Dim(Date_Key),
     CONSTRAINT FK_Purch_Branch FOREIGN KEY (Branch_Key) REFERENCES Branch_Dim(Branch_Key),
     CONSTRAINT FK_Purch_Product FOREIGN KEY (Product_Key) REFERENCES Product_Dim(Product_Key),
     CONSTRAINT FK_Purch_Staff FOREIGN KEY (Staff_Key) REFERENCES Staff_Dim(Staff_Key)
-    CONSTRAINT FK_Purchases_Fact_Purchase_ID FOREIGN KEY (Purchase_ID) REFERENCES Purchase_Orders(PurchaseOrderID)
+    CONSTRAINT FK_Purchases_Fact_Purchase_ID FOREIGN KEY (Purchase_ID) REFERENCES Purchase_Orders(PurchaseOrderID),
+    CONSTRAINT FK_Purch_Received_Date FOREIGN KEY (Received_Date_Key) REFERENCES Date_Dim(Date_Key),
+    CONSTRAINT CK_Purch_Qty_Ordered CHECK (Quantity_Ordered > 0),
+    CONSTRAINT CK_Purch_Qty_Received
+        CHECK (
+            Quantity_Received >= 0
+            AND Quantity_Received <= Quantity_Ordered
+        ),
+    CONSTRAINT CK_Purch_Unit_Cost CHECK (Unit_Cost >= 0),
+    CONSTRAINT CK_Purch_Line_Total CHECK (Line_Total = Quantity_Ordered * Unit_Cost)
 );
 
--- 10. Sales Fact
+-- Sales Fact
 CREATE TABLE Sales_Fact (
-    Date_Key            	NUMBER NOT NULL,
+    SO_Date_Key            	NUMBER NOT NULL,
     Branch_Key          	NUMBER NOT NULL,
     Product_Key         	NUMBER NOT NULL,
     Customer_Key        	NUMBER NOT NULL,
-    Voucher_Key         	NUMBER, -- Nullable as not all sales use vouchers
-    Delivery_Company_Key	NUMBER, -- Nullable as not every single sales require delivery
-    Delivery_Date_Key		NUMBER,
     Staff_Key           	NUMBER NOT NULL,
     Order_ID          	   	NUMBER NOT NULL,
-    Quantity            	NUMBER(10) NOT NULL,
-    Unit_Price          	NUMBER(10,2) NOT NULL,
-    Line_Total         		NUMBER(15,2) NOT NULL,
-    Delivery_Fee        	NUMBER(10,2) NOT NULL,
-    Item_Discount       	NUMBER(10,2) NOT NULL,
-    MyKasih_Subsidy     	NUMBER(10,2) NOT NULL,
-    CONSTRAINT PK_Sales_Fact PRIMARY KEY (Date_Key, Branch_Key, Product_Key, Customer_Key, Staff_Key, Order_ID),
-    CONSTRAINT FK_Sales_Date FOREIGN KEY (Date_Key) REFERENCES Date_Dim(Date_Key),
+    Delivery_Company_ID		VARCHAR2(10),
+    Scheduled_Delivery_Date_Key	NUMBER,
+    Delivered_Date_Key		NUMBER,
+    Quantity            	NUMBER(12) NOT NULL,
+    Unit_Price          	NUMBER(12,2) NOT NULL,
+    MyKasih_Subsidy_Amount	NUMBER(14,2) DEFAULT 0 NOT NULL,
+    Voucher_Discount_Amount 	NUMBER(14,2) DEFAULT 0 NOT NULL, -- Split the amount from Order level to Order Items level
+    Line_Total         		NUMBER(14,2) NOT NULL,
+    Delivery_Fee        	NUMBER(12,2) DEFAULT 0 NOT NULL,
+    City 			VARCHAR2(80),
+    State			VARCHAR2(80),
+    PostCode			NUMBER(5),
+    Order_Status		VARCHAR2(20),
+    CONSTRAINT PK_Sales_Fact PRIMARY KEY (SO_Date_Key, Branch_Key, Product_Key, Customer_Key, Staff_Key, Order_ID),
+    CONSTRAINT FK_Sales_SO_Date FOREIGN KEY (SO_Date_Key) REFERENCES Date_Dim(Date_Key),
     CONSTRAINT FK_Sales_Branch FOREIGN KEY (Branch_Key) REFERENCES Branch_Dim(Branch_Key),
     CONSTRAINT FK_Sales_Product FOREIGN KEY (Product_Key) REFERENCES Product_Dim(Product_Key),
     CONSTRAINT FK_Sales_Customer FOREIGN KEY (Customer_Key) REFERENCES Customer_Dim(Customer_Key),
-    CONSTRAINT FK_Sales_Voucher FOREIGN KEY (Voucher_Key) REFERENCES Voucher_Dim(Voucher_Key),
-    CONSTRAINT FK_Delivery_Company FOREIGN KEY (Delivery_Company_Key) REFERENCES Delivery_Company_Dim(Delivery_Company_Key),
-    CONSTRAINT FK_Delivery_Date FOREIGN KEY (Delivery_Date_Key) REFERENCES Date_Dim(Date_Key),
     CONSTRAINT FK_Sales_Staff FOREIGN KEY (Staff_Key) REFERENCES Staff_Dim(Staff_Key),
-    CONSTRAINT FK_Sales_Fact_Order_ID FOREIGN KEY (Order_ID) REFERENCES Orders(OrderID)
+    CONSTRAINT FK_Sales_Fact_Order_ID FOREIGN KEY (Order_ID) REFERENCES Orders(OrderID),
+    CONSTRAINT FK_Scheduled_Delivery_Date FOREIGN KEY (Scheduled_Delivery_Date_Key) REFERENCES Date_Dim(Date_Key),
+    CONSTRAINT FK_Delivered_Date FOREIGN KEY (Delivered_Date_Key) REFERENCES Date_Dim(Date_Key),
+    CONSTRAINT CK_Sales_Qty CHECK (Quantity > 0),
+    CONSTRAINT CK_Sales_Unit_Price CHECK (Unit_Price >= 0),
+    CONSTRAINT CK_Sales_MyKasih_Subsidy_Amount
+        CHECK (
+            MyKasih_Subsidy_Amount >= 0
+            AND MyKasih_Subsidy_Amount <= (Quantity * Unit_Price) - Voucher_Discount_Amount
+        ),
+    CONSTRAINT CK_Sales_Voucher_Discount_Amount
+        CHECK (
+            Voucher_Discount_Amount >= 0
+            AND Voucher_Discount_Amount <= Quantity * Unit_Price
+        ),
+    CONSTRAINT CK_Sales_Line_Total
+        CHECK (
+            Line_Total =
+                (Quantity * Unit_Price) - Voucher_Discount_Amount - MyKasih_Subsidy_Amount
+        ),
+    CONSTRAINT CK_Sales_Delivery_Fee CHECK (Delivery_Fee >= 0),
+    CONSTRAINT CK_Sales_PostCode CHECK (REGEXP_LIKE(PostCode, '^[0-9]{5}$')),
+    CONSTRAINT CK_Sales_Order_Status CHECK (
+    	Order_Status IS NULL
+    	OR Order_Status IN ('PENDING', 'PICKED_UP', 'DELIVERED', 'FAILED')
+    )
 );
 
--- 11. Returns Fact
+-- Returns Fact
 CREATE TABLE Returns_Fact (
-    Processed_Date_Key	NUMBER NOT NULL,
+    Request_Date_Key	NUMBER NOT NULL,
     Branch_Key          NUMBER NOT NULL,
     Product_Key         NUMBER NOT NULL,
     Customer_Key        NUMBER NOT NULL,
     Staff_Key           NUMBER NOT NULL,
     Return_ID           NUMBER NOT NULL,
+    Processed_Date_Key  NUMBER,
     Quantity_Returned   NUMBER(10) NOT NULL,
+    Resolution_Type 	VARCHAR2(15),
     Refund_Amount       NUMBER(15,2) NOT NULL,
-    Return_Reason       VARCHAR2(255),
-    CONSTRAINT PK_Returns_Fact PRIMARY KEY (Processed_Date_Key, Branch_Key, Customer_Key, Staff_Key, Product_Key, Return_ID),
-    CONSTRAINT FK_Return_Date FOREIGN KEY (Processed_Date_Key) REFERENCES Date_Dim(Date_Key),
+    Return_Reason       VARCHAR2(20) NOT NULL,
+    Return_Status	VARCHAR2(15) DEFAULT 'PENDING' NOT NULL,
+    CONSTRAINT PK_Returns_Fact PRIMARY KEY (Request_Date_Key, Branch_Key, Customer_Key, Staff_Key, Product_Key, Return_ID),
+    CONSTRAINT FK_Return_Date FOREIGN KEY (Request_Date_Key) REFERENCES Date_Dim(Date_Key),
     CONSTRAINT FK_Return_Branch FOREIGN KEY (Branch_Key) REFERENCES Branch_Dim(Branch_Key),
     CONSTRAINT FK_Return_Product FOREIGN KEY (Product_Key) REFERENCES Product_Dim(Product_Key),
     CONSTRAINT FK_Return_Customer FOREIGN KEY (Customer_Key) REFERENCES Customer_Dim(Customer_Key),
     CONSTRAINT FK_Return_Staff FOREIGN KEY (Staff_Key) REFERENCES Staff_Dim(Staff_Key),
-    CONSTRAINT FK_Returns_Fact_Return_ID FOREIGN KEY (Return_ID) REFERENCES Return_Requests(ReturnID)
+    CONSTRAINT FK_Returns_Fact_Return_ID FOREIGN KEY (Return_ID) REFERENCES Return_Requests(ReturnID),
+    CONSTRAINT FK_Return_Processed_Date FOREIGN KEY (Processed_Date_Key) REFERENCES Date_Dim(Date_Key),
+    CONSTRAINT CK_Return_Quantity_Returned CHECK (Quantity_Returned >= 0),
+    CONSTRAINT CK_Return_Resolution_Type CHECK (
+            ResolutionType IS NULL
+            OR ResolutionType IN ('REFUND', 'REPLACE', 'REJECT')
+        ),
+    CONSTRAINT CK_Return_Refund_Amount
+        CHECK (
+            Refund_Amount IS NULL
+            OR Refund_Amount >= 0
+        ),
+    CONSTRAINT CK_Return_Reason 
+        CHECK (Return_Reason IN ('MISSING', 'BROKEN', 'EXPIRED', 'WRONG_ITEM', 'OTHER')),
+    CONSTRAINT CK_Return_Status
+        CHECK (Status IN ('PENDING', 'APPROVED', 'REJECTED', 'COMPLETED', 'CANCELLED'))
 );
 
--- 12. Inventory Fact
-CREATE TABLE Inventory_Fact (
-    Snapshot_Date_Key   NUMBER NOT NULL,
+-- Stock Movement Fact
+CREATE TABLE Stock_Movement_Fact (
+    Movement_Date_Key   NUMBER NOT NULL,
     Product_Key         NUMBER NOT NULL,
     Branch_Key          NUMBER NOT NULL,
-    Quantity_On_Hand    NUMBER(10) NOT NULL,
-    Reorder_Level       NUMBER(10) NOT NULL,
-    Reorder_Quantity    NUMBER(10) NOT NULL,
-    CONSTRAINT PK_Inventory_Fact PRIMARY KEY (Snapshot_Date_Key, Product_Key, Branch_Key),
-    CONSTRAINT FK_Inv_Date FOREIGN KEY (Snapshot_Date_Key) REFERENCES Date_Dim(Date_Key),
-    CONSTRAINT FK_Inv_Product FOREIGN KEY (Product_Key) REFERENCES Product_Dim(Product_Key),
-    CONSTRAINT FK_Inv_Branch FOREIGN KEY (Branch_Key) REFERENCES Branch_Dim(Branch_Key)
+    Staff_Key 		NUMBER NOT NULL,
+    Movement_Type	VARCHAR2(15) NOT NULL,
+    Reference_ID	VARCHAR2(10) NOT NULL,
+    Quantity_On_Hand    NUMBER(12) NOT NULL,
+    Quantity_In         NUMBER(12) NOT NULL,
+    Quantity_Out        NUMBER(12) NOT NULL,
+    CONSTRAINT PK_Stock_Fact PRIMARY KEY (Movement_Date_Key, Product_Key, Branch_Key, Staff_Key),
+    CONSTRAINT FK_Stock_Date FOREIGN KEY (Movement_Date_Key) REFERENCES Date_Dim(Date_Key),
+    CONSTRAINT FK_Stock_Product FOREIGN KEY (Product_Key) REFERENCES Product_Dim(Product_Key),
+    CONSTRAINT FK_Stock_Branch FOREIGN KEY (Branch_Key) REFERENCES Branch_Dim(Branch_Key),
+    CONSTRAINT FK_Stock_Staff FOREIGN KEY (Staff_Key) REFERENCES Staff_Dim(Staff_Key),
+    CONSTRAINT CK_Stock_Quantity_On_Hand CHECK (Quantity_On_Hand >= 0),
+    CONSTRAINT CK_Stock_Quantity_In CHECK (Quantity_In >= 0),
+    CONSTRAINT CK_Stock_Quantity_Out CHECK (Quantity_Out >= 0)
+    -- Update constraints for Movement_Type and link foreign key for Reference_ID
 );
