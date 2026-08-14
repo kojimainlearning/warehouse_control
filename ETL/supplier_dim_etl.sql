199727d03b98b6e9d1358c9ee5ee70ea14a2d6a6
WANJIN1080 <138848954+WANJIN1080@users.noreply.github.com>
2026-08-14 20:13:26 +0800

    Add files via upload

---
+++ ETL/supplier_dim_etl.sql
@@ -0,0 +1,46 @@
+CREATE SEQUENCE SEQ_SUPPLIER_DIM
+START WITH 1
+INCREMENT BY 1;
+
+MERGE INTO Supplier_Dim T
+USING (
+    SELECT
+        SupplierID,
+        SupplierName,
+        Status,
+        City,
+        State,
+        PostCode
+    FROM Suppliers
+) S
+ON (T.Supplier_ID = S.SupplierID)
+
+WHEN MATCHED THEN
+    UPDATE SET
+        T.Supplier_Name   = S.SupplierName,
+        T.Supplier_Status = S.Status,
+        T.City            = S.City,
+        T.State           = S.State,
+        T.PostCode        = S.PostCode
+
+WHEN NOT MATCHED THEN
+    INSERT (
+        Supplier_Key,
+        Supplier_ID,
+        Supplier_Name,
+        Supplier_Status,
+        City,
+        State,
+        PostCode
+    )
+    VALUES (
+        SEQ_SUPPLIER_DIM.NEXTVAL,
+        S.SupplierID,
+        S.SupplierName,
+        S.Status,
+        S.City,
+        S.State,
+        S.PostCode
+    );
+
+COMMIT;
